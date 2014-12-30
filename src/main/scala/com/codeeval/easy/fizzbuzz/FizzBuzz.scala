bc91a43c05200d625a84fc129cde9a1623888758
“Piotr <piotr.kacprzak11@gmail.com>
2014-12-30 13:08:07 +0000

    new definition for matcher

--- src/main/scala/com/codeeval/easy/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/com/codeeval/easy/fizzbuzz/FizzBuzz.scala
@@ -4,19 +4,19 @@ package com.codeeval.easy.fizzbuzz
  * Created by paverell on 29.12.14.
  */
 class FizzBuzz {
-  def convert(ints: List[Int]): List[String] = {
+  def convert(input: List[Int]): List[String] = {
     def convertImpl(range: List[Int], acc: List[String]): List[String] =
       if (range.isEmpty) acc
-      else convertImpl(range.tail, acc ::: List(matcher(range.head, ints(0), ints(1))))
+      else convertImpl(range.tail, acc ::: List(matcher(range.head)))
 
-    def matcher(numberToMatch: Int, first: Int, second: Int): String = {
+    def matcher(numberToMatch: Int): String = {
       numberToMatch match {
-        case x if x % first == 0 => if(x % second == 0) "FB" else "F"
-        case x if x % first != 0 => if(x % second == 0) "B" else numberToMatch.toString
+        case x if x % input(0) == 0 => if(x % input(1) == 0) "FB" else "F"
+        case x if x % input(0) != 0 => if(x % input(1) == 0) "B" else x.toString
       }
     }
 
-    convertImpl(List.range(1, ints(2) + 1), List())
+    convertImpl(List.range(1, input(2) + 1), List())
   }
 
 }

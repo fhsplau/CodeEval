f13d9a5d2545ffa45fa7fdb7300ab8628d94c39d
“Piotr <piotr.kacprzak11@gmail.com>
2014-12-30 14:17:37 +0000

    implicit class changed to implicit function

--- src/main/scala/com/codeeval/easy/fizzbuzz/FizzBuzzImplicit.scala
+++ src/main/scala/com/codeeval/easy/fizzbuzz/FizzBuzzImplicit.scala
@@ -1,23 +1,23 @@
 package com.codeeval.easy.fizzbuzz
 
+class FizzImplicit(input: List[Int]) {
+  def convert: List[String] = {
+    def convertImpl(range: List[Int], acc: List[String]): List[String] =
+      if (range.isEmpty) acc
+      else convertImpl(range.tail, acc ::: List(matcher(range.head)))
+
+    def matcher(numberToMatch: Int): String = {
+      numberToMatch match {
+        case x if x % input(0) == 0 => if (x % input(1) == 0) "FB" else "F"
+        case x if x % input(0) != 0 => if (x % input(1) == 0) "B" else x.toString
+      }
+    }
+
+    convertImpl(List.range(1, input(2) + 1), List())
+  }
+}
 
 object FizzBuzzImplicit {
 
-  implicit class FizzImplicit(input: List[Int]) {
-    def convert: List[String] = {
-      def convertImpl(range: List[Int], acc: List[String]): List[String] =
-        if (range.isEmpty) acc
-        else convertImpl(range.tail, acc ::: List(matcher(range.head)))
-
-      def matcher(numberToMatch: Int): String = {
-        numberToMatch match {
-          case x if x % input(0) == 0 => if (x % input(1) == 0) "FB" else "F"
-          case x if x % input(0) != 0 => if (x % input(1) == 0) "B" else x.toString
-        }
-      }
-
-      convertImpl(List.range(1, input(2) + 1), List())
-    }
-
-  }
+  implicit def implicitConvert(input: List[Int]): FizzImplicit = new FizzImplicit(input)
 }

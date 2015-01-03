1b7e7a61eb90697d1802eee9a54ec9637b0ac9fa
“Piotr <piotr.kacprzak11@gmail.com>
2015-01-03 16:17:28 +0000

    added auxiliary constructors

--- src/main/scala/com/codeeval/easy/fizzbuzz/FizzBuzzMap.scala
+++ src/main/scala/com/codeeval/easy/fizzbuzz/FizzBuzzMap.scala
@@ -1,13 +1,15 @@
 package com.codeeval.easy.fizzbuzz
 
-class FizzBuzzMap {
+class FizzBuzzMap(divByFirst: String, divBySecond: String, divByBoth: String) {
 
-  val divByFirst: String = "F"
-  val divBySecond: String = "B"
-  val divByBoth: String = "FB"
+  def this() = this("F","B","FB")
+  def this(divByFirst: String) = this(divByFirst,"B","FB")
+  def this(divByFirst: String, divBySecond:String) = this(divByFirst,divBySecond,"FB")
 
-  def convert(inputList: List[Int]): List[String] = List.range(1, inputList.last + 1).map {
-    case x if x % inputList.head == 0 => if (x % inputList(1) == 0) divByBoth else divByFirst
-    case x if x % inputList.head != 0 => if (x % inputList(1) == 0) divBySecond else x.toString
-  }
+  def convert(inputList: List[Int]): List[String] =
+    if (inputList.size > 3) throw new Exception
+    else List.range(1, inputList.last + 1).map {
+      case x if x % inputList.head == 0 => if (x % inputList(1) == 0) divByBoth else divByFirst
+      case x if x % inputList.head != 0 => if (x % inputList(1) == 0) divBySecond else x.toString
+    }
 }

411b869702bcd1473f0f7e0255b08e0e03b076d1
“Piotr <piotr.kacprzak11@gmail.com>
2015-01-03 14:11:14 +0000

    added trait and abstract class

--- src/main/scala/com/codeeval/easy/fizzbuzz/FizzBuzzImplicit.scala
+++ src/main/scala/com/codeeval/easy/fizzbuzz/FizzBuzzImplicit.scala
@@ -2,20 +2,26 @@ package com.codeeval.easy.fizzbuzz
 
 import scala.collection.mutable.ListBuffer
 
-class FizzImplicit(input: List[Int]) {
-  def convert: List[String] = {
-    def convertImpl(range: List[Int], acc: ListBuffer[String]): ListBuffer[String] =
-      if (range.isEmpty) acc
-      else convertImpl(range.tail, acc += matcher(range.head))
+trait FizzTrait {
+  def convert: List[String]
+}
 
-    def matcher(numberToMatch: Int): String = {
-      numberToMatch match {
-        case x if x % input(0) == 0 => if (x % input(1) == 0) "FB" else "F"
-        case x if x % input(0) != 0 => if (x % input(1) == 0) "B" else x.toString
-      }
+abstract class FizzAbstract(input: List[Int]) extends FizzTrait {
+  override def convert: List[String] = convertImpl(List.range(1, input(2) + 1), ListBuffer()).toList
+
+  def convertImpl(range: List[Int], acc: ListBuffer[String]): ListBuffer[String]
+}
+
+class FizzImplicit(input: List[Int]) extends FizzAbstract(input) {
+  def convertImpl(range: List[Int], acc: ListBuffer[String]): ListBuffer[String] =
+    if (range.isEmpty) acc
+    else convertImpl(range.tail, acc += matcher(range.head))
+
+  def matcher(numberToMatch: Int): String = {
+    numberToMatch match {
+      case x if x % input(0) == 0 => if (x % input(1) == 0) "FB" else "F"
+      case x if x % input(0) != 0 => if (x % input(1) == 0) "B" else x.toString
     }
-
-    convertImpl(List.range(1, input(2) + 1), new ListBuffer()).toList
   }
 }
 

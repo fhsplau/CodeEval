cb58906b70cbbd5f5ae35d7ea5f8e465dfa00046
“Piotr <piotr.kacprzak11@gmail.com>
2015-01-03 16:17:59 +0000

    new test for auxiliary constructors

--- src/test/scala/com/codeeval/easy/fizzbuzz/FizzBuzzMapTest.scala
+++ src/test/scala/com/codeeval/easy/fizzbuzz/FizzBuzzMapTest.scala
@@ -21,5 +21,21 @@ class FizzBuzzMapTest extends FunSuite with BeforeAndAfter{
     assert(f.convert(List(3,3,10)) === List("1", "2", "FB", "4", "5", "FB", "7", "8", "FB", "10"))
   }
 
+  test("exception if input list's size is bigger than 3") {
+    intercept[Exception] {
+      f.convert(List(1,2,3,10))
+    }
+  }
+
+  test("multiple constructors") {
+    val f1 = new FizzBuzzMap("A","C","AB")
+    val f2 = new FizzBuzzMap("A","C")
+    val f3 = new FizzBuzzMap("A")
+
+    assert(f2.convert(List(3,5,10)) === List("1", "2", "A", "4", "C", "A", "7", "8", "A", "C"))
+    assert(f3.convert(List(3,5,10)) === List("1", "2", "A", "4", "B", "A", "7", "8", "A", "B"))
+    assert(f1.convert(List(3,3,10)) === List("1", "2", "AB", "4", "5", "AB", "7", "8", "AB", "10"))
+  }
+
 
 }

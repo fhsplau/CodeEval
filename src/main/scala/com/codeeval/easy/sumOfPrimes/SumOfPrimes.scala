df50f4887ce0fddebbf6cfa8191333ae57cbfde5
“Piotr <piotr.kacprzak11@gmail.com>
2015-01-04 18:52:36 +0000

    deleted previousNumbers List

--- src/main/scala/com/codeeval/easy/sumOfPrimes/SumOfPrimes.scala
+++ src/main/scala/com/codeeval/easy/sumOfPrimes/SumOfPrimes.scala
@@ -4,15 +4,15 @@ class SumOfPrimes {
   val bound = 1000
 
   def sumPrimes: BigInt = {
-    def sumPrimesImpl(previousNumbers: List[Int], nextNumber: Int, acc: Int, sum: BigInt): BigInt = {
+    def sumPrimesImpl(nextNumber: Int, acc: Int, sum: BigInt): BigInt = {
       if (acc > bound) sum
-      else if (isPrime(nextNumber, previousNumbers)) sumPrimesImpl(previousNumbers ::: List(nextNumber), nextNumber + 1, acc + 1, sum + nextNumber)
-      else sumPrimesImpl(previousNumbers ::: List(nextNumber), nextNumber + 1, acc, sum)
+      else if (isPrime(nextNumber)) sumPrimesImpl(nextNumber + 1, acc + 1, sum + nextNumber)
+      else sumPrimesImpl(nextNumber + 1, acc, sum)
     }
 
-    def isPrime(number: Int, prevNumbers: List[Int]): Boolean = prevNumbers.filter(x => number % x == 0).sum == 0
+    def isPrime(number: Int): Boolean = !List.range(2, number).exists(x => number % x == 0)
 
-    sumPrimesImpl(List(), 2, 1, 0)
+    sumPrimesImpl(4, 3, 5)
   }
 
 }

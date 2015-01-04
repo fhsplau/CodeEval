8ce2ed20f58efee3681d0d229b00b08cdcaf908c
“Piotr <piotr.kacprzak11@gmail.com>
2015-01-04 20:18:37 +0000

    changed List to Array

--- src/main/scala/com/codeeval/easy/reversewords/ReverseWords.scala
+++ src/main/scala/com/codeeval/easy/reversewords/ReverseWords.scala
@@ -4,11 +4,11 @@ class ReverseWords {
   def reverse(sentence: String): String = sentence.split(" ").reverse.mkString(" ")
 
   def reverse2(sentence: String): String = {
-    def reverseImpl(listWithWords: Array[String], acc: List[String]): List[String] = {
+    def reverseImpl(listWithWords: Array[String], acc: Array[String]): Array[String] = {
       if(listWithWords.isEmpty) acc
-      else reverseImpl(listWithWords.tail, List(listWithWords.head) ::: acc)
+      else reverseImpl(listWithWords.tail, Array(listWithWords.head) ++ acc)
     }
 
-    reverseImpl(sentence.split(" "), List()).mkString(" ")
+    reverseImpl(sentence.split(" "), Array()).mkString(" ")
   }
 }

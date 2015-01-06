6de9f1f2f1b24715071c3b0f12fae2e502c5285a
paverell <piotr.kacprzak11@gmail.com>
2015-01-06 18:05:19 +0000

    refactored

--- src/main/scala/com/codeeval/easy/bitpositions/BitPositions.scala
+++ src/main/scala/com/codeeval/easy/bitpositions/BitPositions.scala
@@ -2,8 +2,9 @@ package com.codeeval.easy.bitpositions
 
 class Bits(n: Int) {
   type Bit = Int => Boolean
-  
+
   private def bit(n: Int): Bit = (p: Int) => (n & 1 << p) == (1 << p)
+
   def areBitsOn(p1: Int, p2: Int): Boolean = bit(n)(p1) && bit(n)(p2)
 }
 

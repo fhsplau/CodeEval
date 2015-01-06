5d5b61903450c34a42e56bb4cf73863ffa2c5c37
paverell <piotr.kacprzak11@gmail.com>
2015-01-06 14:35:29 +0000

    second implementation

--- src/main/scala/com/codeeval/easy/multiplesofanumber/Multiples.scala
+++ src/main/scala/com/codeeval/easy/multiplesofanumber/Multiples.scala
@@ -2,14 +2,11 @@ package com.codeeval.easy.multiplesofanumber
 
 class Multiples {
   def multiply(x: Int, n: Int): Int = {
-    def multiplyImpl(prev: Int, next: Int): Int = {
-      if (next >= x) next
-      else multiplyImpl(next, prev + next)
-    }
-
-    def isPowerOfTwo(number: Int): Boolean = (number & (number - 1)) == 0
+    def multiplyImpl(prev: Int, next: Int): Int = if (next >= x) next else multiplyImpl(next, prev + next)
 
     if (isPowerOfTwo(n)) multiplyImpl(n, 2 * n) else throw new Exception
   }
+  def isPowerOfTwo(number: Int): Boolean = (number & (number - 1)) == 0
 
+  def multiply2(x: Int, n: Int): Int = if(isPowerOfTwo(n)) n + n * ((x - n) / n + 1) else throw new Exception
 }
--- src/test/scala/com/codeeval/easy/multiplesofanumber/MultiplesTest.scala
+++ src/test/scala/com/codeeval/easy/multiplesofanumber/MultiplesTest.scala
@@ -5,24 +5,29 @@ import org.scalatest.{BeforeAndAfter, FunSuite}
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class MultiplesTest extends FunSuite with BeforeAndAfter{
-  var m:Multiples = _
+class MultiplesTest extends FunSuite with BeforeAndAfter {
+  var m: Multiples = _
 
   before {
     m = new Multiples
   }
 
   test("13 and 8 will give 16") {
-    assert(m.multiply(13,8) === 16)
+    assert(m.multiply(13, 8) === 16)
+    assert(m.multiply2(13, 8) === 16)
   }
 
   test("100 and 8 will give 104") {
-    assert(m.multiply(100,8) === 104)
+    assert(m.multiply(100, 8) === 104)
+    assert(m.multiply2(100, 8) === 104)
   }
 
   test("exception if n is not a power of 2") {
     intercept[Exception] {
-      m.multiply(100,7)
+      m.multiply(100, 7)
+    }
+    intercept[Exception] {
+      m.multiply2(100, 7)
     }
   }
 

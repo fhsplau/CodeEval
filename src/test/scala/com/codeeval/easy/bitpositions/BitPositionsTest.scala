a37fafdcf301d36aec82658531c598ac5564f6bc
paverell <piotr.kacprzak11@gmail.com>
2015-01-06 18:05:30 +0000

    refactored

--- src/test/scala/com/codeeval/easy/bitpositions/BitPositionsTest.scala
+++ src/test/scala/com/codeeval/easy/bitpositions/BitPositionsTest.scala
@@ -6,14 +6,14 @@ import org.scalatest.junit.JUnitRunner
 import com.codeeval.easy.bitpositions.Bits._
 
 @RunWith(classOf[JUnitRunner])
-class BitPositionsTest extends FunSuite with BeforeAndAfter{
+class BitPositionsTest extends FunSuite with BeforeAndAfter {
 
 
-  test("bit 2 and 3 are on in 86"){
-    assert(86.areBitsOn(2,4))
+  test("bit 2 and 3 are on in 86") {
+    assert(86.areBitsOn(2, 4))
   }
 
-  test("bit 1 and 2 are off in 125"){
-    assert(!125.areBitsOn(1,2))
+  test("bit 1 and 2 are off in 125") {
+    assert(!125.areBitsOn(1, 2))
   }
 }

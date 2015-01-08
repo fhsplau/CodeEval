d210d6d36a9837efc54f82359512305a95857577
paverell <piotr.kacprzak11@gmail.com>
2015-01-08 23:18:55 +0000

    cleaned

--- src/main/scala/com/codeeval/easy/bitpositions/BitPositions.scala
+++ src/main/scala/com/codeeval/easy/bitpositions/BitPositions.scala
@@ -4,23 +4,22 @@ abstract class Bit(n: Int) {
 
   type Bit = () => Int
 
-  def bit(p: Int): Bit = () => if((n & 1 << p) == (1 << p)) 1 else 0
+  def bit(p: Int): Bit = () => if ((n & 1 << p) == (1 << p)) 1 else 0
 
   def areBitsOn(ps: Int*): Boolean
 
   def bitOn(p: Int): Int = if (bit(p)() == 1) n else n | 1 << p
 
-  def bitOff(p: Int): Int = if (bit(p)()!=1) n else n ^ 1 << p
+  def bitOff(p: Int): Int = if (bit(p)() != 1) n else n ^ 1 << p
 }
 
-class BitLogic(b: () => Int){
-  def isOn: Boolean = b()==1
+class BitLogic(b: () => Int) {
+  def isOn: Boolean = b() == 1
 }
 
 class Bin(n: Int) extends Bit(n) {
-  type Bin = List[Bit] => String
 
-  def areBitsOn(ps: Int*): Boolean = if (ps.isEmpty) true else (bit(ps.head)()==1) && areBitsOn(ps.tail: _*)
+  def areBitsOn(ps: Int*): Boolean = if (ps.isEmpty) true else (bit(ps.head)() == 1) && areBitsOn(ps.tail: _*)
 
   def numOfBits: Int = {
     def numOfBitsImpl(num: Int, acc: Int): Int = if (num == 0) acc else numOfBitsImpl(num >> 1, acc + 1)
@@ -29,18 +28,19 @@ class Bin(n: Int) extends Bit(n) {
   }
 
   private def areBitsOnList(ps: List[Int]): List[Boolean] = ???
-//    if (ps.nonEmpty) List(bit(ps.head)) ::: areBitsOnList(ps.tail)
-//    else List()
+
+  //    if (ps.nonEmpty) List(bit(ps.head)) ::: areBitsOnList(ps.tail)
+  //    else List()
 
   def convertToBin: String = areBitsOnList(List.range(0, numOfBits)).reverse.map {
     case true => "1"
     case false => "0"
   }.mkString
 
-  def bin: Bin = ???
 }
 
 object BinaryOperations {
   implicit def binImpl(n: Int): Bin = new Bin(n)
+
   implicit def logic(b: () => Int): BitLogic = new BitLogic(b)
 }
